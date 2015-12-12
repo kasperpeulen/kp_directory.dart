cc960d1511dd0160bcd8a3e62caadbc17b54b482
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-12 22:32:09 +0100

    refactor: refactor tests to use setUp and tearDown

--- test/kp_directory_test.dart
+++ test/kp_directory_test.dart
@@ -5,11 +5,18 @@ import 'package:path/path.dart';
 import 'package:test/test.dart';
 
 void main() {
+  var tempDir;
+
+  setUp(() {
+    tempDir = Directory.systemTemp.createTempSync();
+  });
+
+  tearDown(() {
+    tempDir.deleteSync(recursive: true);
+  });
+
   group("makeEmptySync", () {
     test("it makes the directory empty", () {
-      // make a tempdir
-      final tempDir = Directory.systemTemp.createTempSync();
-
       // make sure it is empty
       expect(tempDir.listSync().isEmpty, isTrue);
 
@@ -20,33 +27,25 @@ void main() {
 
       makeEmptySync(tempDir);
       expect(tempDir.listSync().isEmpty, isTrue);
-      tempDir.deleteSync(recursive: true);
     });
   });
 
   group("createFileSync", () {
     test("it creates a file", () {
-      final tempDir = Directory.systemTemp.createTempSync();
-
       final file = getFile(tempDir, 'hello_world.txt')..createSync();
       expect(file.existsSync(), isTrue);
-      tempDir.deleteSync(recursive: true);
     });
   });
 
   group("createDirSync", () {
     test("it creates a dir", () {
-      final tempDir = Directory.systemTemp.createTempSync();
-
       final dir = getDirectory(tempDir, 'hello_world')..createSync();
       expect(dir.existsSync(), isTrue);
-      tempDir.deleteSync(recursive: true);
     });
   });
 
   group("copySync", () {
     test("it does copy", () {
-      final tempDir = Directory.systemTemp.createTempSync();
       final fileInTempDir = getFile(tempDir, 'hello_world.txt')..createSync();
 
       final newDir = Directory.systemTemp.createTempSync();
@@ -59,7 +58,6 @@ void main() {
 
       expect(basename(newDir.listSync()[0].path), basename(fileInTempDir.path));
       newDir.deleteSync(recursive: true);
-      tempDir.deleteSync(recursive: true);
     });
   });
 }
