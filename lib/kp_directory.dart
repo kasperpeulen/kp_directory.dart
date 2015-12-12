139d387a4dabf6b1392505c89862f25c2999048b
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-12 21:28:18 +0100

    feat: add functions: emptySync, createFileSync, createDirSync, copySync

--- lib/kp_directory.dart
+++ lib/kp_directory.dart
@@ -1,1 +1,33 @@
+import 'dart:io';
 
+import 'package:path/path.dart';
+
+void makeEmptySync(Directory dir) {
+  dir.listSync().forEach((f) => f.deleteSync(recursive: true));
+}
+
+File createFileSync(Directory dir, String fileName) {
+  return new File(join(dir.path, fileName))..createSync();
+}
+
+Directory createDirSync(Directory dir, String directoryName) {
+  return new Directory(join(dir.path, directoryName))..createSync();
+}
+
+void copySync(Directory from, Directory to) {
+  if (!to.existsSync()) to.createSync(recursive: true);
+
+  // recursively copy all files and directories
+  from.listSync().forEach((element) {
+    String newPath = "${to.path}/${basename(element.path)}";
+    if (element is File) {
+      element.copy(newPath);
+    } else if (element is Directory) {
+      copySync(element, new Directory(newPath));
+    } else if (element is Link) {
+      throw new UnimplementedError(
+          'Copying directories that contain Link objects '
+          'is not implemented yet');
+    }
+  });
+}
