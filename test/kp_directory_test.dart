148c3444ec3191c2bd370ba378560f000555732d
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-12 21:28:42 +0100

    test: add test for makeEmptySync

--- test/kp_directory_test.dart
+++ test/kp_directory_test.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'dart:io';
+
+import 'package:kp_directory/kp_directory.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("makeEmptySync", () {
+    test("it makes the directory empty", () {
+      // make a tempdir
+      final tempDir = Directory.systemTemp.createTempSync();
+
+      // make sure it is empty
+      expect(tempDir.listSync().isEmpty, isTrue);
+
+      // populate it
+      createFileSync(tempDir, 'file.txt');
+      createDirSync(tempDir, 'my_dir_name');
+      expect(tempDir.listSync().isEmpty, isFalse);
+
+      makeEmptySync(tempDir);
+      expect(tempDir.listSync().isEmpty, isTrue);
+    });
+  });
+}
